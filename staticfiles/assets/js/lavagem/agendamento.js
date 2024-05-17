cbddec41a97d61504fbdbced0773ab9723b7bc93
Dev-Andersonsena <dev.fullstack.anderson@gmail.com>
2024-05-17 00:37:05 -0300

    atualização do collectsttatic

--- staticfiles/assets/js/lavagem/agendamento.js
+++ staticfiles/assets/js/lavagem/agendamento.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {
- 
-    // Fakes the loading setting a timeout
-      setTimeout(function() {
-          $('body').addClass('loaded');
-      }, 3500);
-   
-  });
+(function($){
+    $('.trigger').click(function(){
+      $('#ajaxloader1, .outer, .inner, .barlittle, .pointcircle, .bar, #loadpulse div, #shadowloader span, .loadingtext span').toggleClass('stop');
+    });
+  })(jQuery);
+
